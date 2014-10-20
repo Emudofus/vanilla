1a3329bf385ea06cea407d81f4ec94f881892547
Blackrush <blackrushx@gmail.com>
2014-10-20 16:33:06 +0200

    fix test

--- test/vanilla/frontend/tokenizer_test.exs
+++ test/vanilla/frontend/tokenizer_test.exs
@@ -4,6 +4,6 @@ defmodule Vanilla.Frontend.RanchTest do
   import Vanilla.Frontend.Tokenizer
 
   test "parse_all" do
-    assert tokenize_all(0, "1.29.1\n\0ab\n#1cd\n\0Af\n\0") == {2, [:version, {:auth, "ab", "cd"}, :get_queue]}
+    assert tokenize_all(0, "1.29.1\n\0ab\n#1cd\n\0Af\n\0") == {2, [{:version, "1.29.1"}, {:auth, "ab", "cd"}, :get_queue]}
   end
 end
